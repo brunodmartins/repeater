ee123a280c375b8cd4feb2d3125e7d7b55a6eafe
brunodmartins <bdm2943@icloud.com>
2024-10-03 23:31:32 -0300

    Adding CSV support

--- repeater.py
+++ repeater.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import subprocess
 
 
@@ -6,14 +7,32 @@ def read_all_lines(data_path):
     with open(data_path, 'r') as f:
             return f.readlines()
 
+def read_csv_data(data_path):
+    result = []
+    with open(data_path, 'r', newline='') as csvfile:
+        for row in csv.DictReader(csvfile):
+            result.append(row)
+    return result
+
 def is_txt_data(data_path):
     return ".txt" in data_path
+
+def is_csv_data(data_path):
+    return ".csv" in data_path
 
 def execute_command(command, data_path, verbose=False):
     if is_txt_data(data_path):
         data = read_all_lines(data_path)
         for line in data:
             subprocess.run([command, line.strip()])
+    elif is_csv_data(data_path):
+        data = read_csv_data(data_path)
+        for row in data:
+            final_command = command
+            for key in row.keys():
+                pattern = "{" + key + "}"
+                final_command = final_command.replace(pattern, row[key])
+            result = subprocess.run([final_command], shell=True, stdout=subprocess.PIPE)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Required arguments")
